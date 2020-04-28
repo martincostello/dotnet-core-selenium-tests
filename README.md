bf59b48b99f816c2666ea8f03cd596ffaff06802
martincostello <martin@martincostello.com>
2020-04-28 11:47:49 +0100

    Update to .NET Core 3.1
    Update the solution to use .NET Core 3.1 and the latest relevant NuGet packages.

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # .NET Core Selenium Tests
 
-A simple test implemented in .NET Core that uses a .NET Core-compatible version of Selenium with multiple browsers.
+A simple test implemented in .NET Core 3.1 that uses Selenium WebDriver with multiple browsers.
 
 ```cmd
 git clone https://github.com/martincostello/dotnet-core-selenium-tests.git
 cd dotnet-core-selenium-tests
-dotnet restore
-dotnet test .\seleniumtests\seleniumtests.csproj
+dotnet test
 ```
--- seleniumtests/BingTests.cs
+++ seleniumtests/BingTests.cs
@@ -1,5 +1,9 @@
 using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 using Xunit;
 
 namespace seleniumtests
@@ -10,38 +14,27 @@ namespace seleniumtests
         [InlineData("Chrome")]
         [InlineData("Edge")]
         [InlineData("Firefox")]
-        [InlineData("InternetExplorer", Skip = "Leaves it open when finished. *bin emoji*")] // Follow https://stackoverflow.com/a/36836137/1064169 for one-time setup
+        [InlineData("InternetExplorer")] // Follow https://stackoverflow.com/a/36836137/1064169 for one-time setup
         public static void Search_For_DotNet_Core(string browserName)
         {
             string url = "https://bing.com"; // Google doesn't seem to work properly in IE at the moment...
 
-            using (var driver = CreateWebDriver(browserName))
-            {
-                driver.Navigate().GoToUrl(url);
-                driver.FindElement(By.Name("q")).SendKeys("dotnet core" + Keys.Enter);
-            }
+            using var driver = CreateWebDriver(browserName);
+
+            driver.Navigate().GoToUrl(url);
+            driver.FindElement(By.Name("q")).SendKeys("dotnet core" + Keys.Enter);
         }
 
         private static IWebDriver CreateWebDriver(string browserName)
         {
-            switch (browserName.ToLowerInvariant())
+            return browserName.ToLowerInvariant() switch
             {
-                case "chrome":
-                    return new OpenQA.Selenium.Chrome.ChromeDriver();
-
-                case "edge":
-                    return new OpenQA.Selenium.Edge.EdgeDriver();
-
-                case "firefox":
-                    return new OpenQA.Selenium.Firefox.FirefoxDriver();
-
-                case "internetexplorer":
-                    var options = new OpenQA.Selenium.IE.InternetExplorerOptions() { IgnoreZoomLevel = true };
-                    return new OpenQA.Selenium.IE.InternetExplorerDriver(options);
-
-                default:
-                    throw new NotSupportedException($"The browser '{browserName}' is not supported.");
-            }
+                "chrome" => new ChromeDriver(),
+                "edge" => new EdgeDriver(),
+                "firefox" => new FirefoxDriver(),
+                "internetexplorer" => new InternetExplorerDriver(new InternetExplorerOptions() { IgnoreZoomLevel = true }),
+                _ => throw new NotSupportedException($"The browser '{browserName}' is not supported."),
+            };
         }
     }
 }
